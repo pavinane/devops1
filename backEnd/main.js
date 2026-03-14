394ce812a4ab86330bc618631f2460095e8c0b20
unknown <pavimegan77@gmail.com>
2026-03-14 16:18:25 +0530

    add backend

---
+++ backEnd/main.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config()
+
+connectDB();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
